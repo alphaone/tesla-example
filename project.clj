f0722b05e5fe6dc821082f5d94e73502c6ca5b3e
Kai Brandes <kai.brandes@otto.de>
2015-03-05 15:19:26 +0100

    fix|kb|fix for broken system

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [de.otto/tesla-microservice "0.1.6"]]
   :main de.otto.tesla.example.example-system
 )
